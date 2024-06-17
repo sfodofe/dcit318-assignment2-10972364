6c24d024899fefd95cd8cc0d00857a1ab09b5579
sfodofe <sfodofe@st.ug.edu.gh>
2024-06-17 14:27:25 +0000

    Pushed Interfaces

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Interface
+public interface IMovable
+{
+    void Move();
+}
+
+// Class that implements IMovable
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Class that implements IMovable
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
