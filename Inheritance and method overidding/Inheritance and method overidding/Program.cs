95d93bbbd5c8b3b06faa795ef1f1f3774910cbf7
sfodofe <sfodofe@st.ug.edu.gh>
2024-06-17 14:18:37 +0000

    Pushed Inheritance and method overriding

---
+++ Inheritance and method overidding/Inheritance and method overidding/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Base class
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        animal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
